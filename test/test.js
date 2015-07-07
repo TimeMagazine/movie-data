d517357f822e170b819fe0f9f49f86d2ac1084d2
Chris Wilson <christopher.e.wilson@gmail.com>
2015-07-07 08:08:39 -0400

    combining keyword search into list function

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 var imdb = require("../lib/imdb");
 
+/*
 imdb.actor({ name: "Benedict Cumberbatch" }, function(actor) {
 	//console.log(actor);
 });
@@ -7,4 +8,10 @@ imdb.actor({ name: "Benedict Cumberbatch" }, function(actor) {
 imdb.by_keyword("sequel", function(movies) {
 	console.log(movies);
 });
+*/
 
+imdb.list.keyword("sequel", function(movie) {
+	console.log(movie);
+}, function(movies) {
+	return movies.length > 250;
+});
